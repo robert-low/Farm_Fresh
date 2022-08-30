42ca33669d2c72c5a1334c2bb9c8af1265e3a6fe
Oliver Atkins <ollieatkins8@outlook.com>
2022-08-30 12:39:03 +0100

    seeded database with faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,22 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+puts "Cleaning database ..."
+Product.destroy_all
+puts "Creating products ..."
+
+20.times do
+  product = Product.create(
+    name: Faker::Food.spice,
+    overview: Faker::Food.ethnic_category,
+    price: Faker::Number.decimal(l_digits: 2),
+    image_url: "https://picsum.photos/seed/picsum/200/300",
+    location: Faker::Address.full_address
+  )
+  puts "product with id: #{product.id} has been created!"
+end
+
+puts "Products created!"
