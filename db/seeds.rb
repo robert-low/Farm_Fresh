114df73fea6bcb9223bf8f9bf040f06b61ef0b49
Oliver Atkins <ollieatkins8@outlook.com>
2022-08-30 12:53:11 +0100

    created seed file with users and products

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,15 +10,39 @@ require 'faker'
 
 puts "Cleaning database ..."
 Product.destroy_all
+User.destroy_all
 puts "Creating products ..."
 
-20.times do
-  product = Product.create(
+user_1 = User.create!(
+  email: "example@example.com",
+  password: "123456"
+)
+
+user_2 = User.create!(
+  email: "example_2@example_2.com",
+  password: "123456"
+)
+
+10.times do
+  product = Product.create!(
     name: Faker::Food.spice,
     overview: Faker::Food.ethnic_category,
     price: Faker::Number.decimal(l_digits: 2),
     image_url: "https://picsum.photos/seed/picsum/200/300",
-    location: Faker::Address.full_address
+    location: Faker::Address.full_address,
+    user: user_1
+  )
+  puts "product with id: #{product.id} has been created!"
+end
+
+10.times do
+  product = Product.create!(
+    name: Faker::Food.spice,
+    overview: Faker::Food.ethnic_category,
+    price: Faker::Number.decimal(l_digits: 2),
+    image_url: "https://picsum.photos/seed/picsum/200/300",
+    location: Faker::Address.full_address,
+    user: user_2
   )
   puts "product with id: #{product.id} has been created!"
 end
