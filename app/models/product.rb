e160c8913bdcd876e15231bddcf5bd1ac892605c
Robert Low <robert.low91@gmail.com>
2022-09-01 12:23:46 +0100

    Geocoding map added, needs formatting

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,7 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :transactions
   has_one_attached :image_url
+
+  geocoded_by :location
+  after_validation :geocode, if: :will_save_change_to_location?
 end
