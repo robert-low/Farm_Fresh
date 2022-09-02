a87eb177c549cfacc10e373369f41682b67d8c32
Alex DiCarlo <alexdicarlo.biz@gmail.com>
2022-09-02 11:45:42 +0100

    finished update to model products

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,13 @@ class Product < ApplicationRecord
   has_many :transactions
   has_one_attached :image_url
 
+  validates :name, presence: true, length: { maximum: 15 }
+  validates :mini_description, presence: true, length: { maximum: 25 }
+  validates :overview, presence: true
+  validates :price, presence: true, length: { maximum: 8 }
+  validates :image_url, presence: true
+  validates :location, presence: true
+
   geocoded_by :location
   after_validation :geocode, if: :will_save_change_to_location?
 
