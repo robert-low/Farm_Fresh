ca053441365b1193668d5054cb04493db9af22fa
Alex DiCarlo <alexdicarlo.biz@gmail.com>
2022-09-02 13:56:17 +0100

    finished homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "products#index"
+  root to: "pages#home"
 
   resources :products do
     resources :transactions, except: :destroy
   end
-resources :users, only: [:show]
-resources :transactions, only: [:destroy]
+  resources :users, only: [:show]
+  resources :transactions, only: [:destroy]
 end
-
 
 # //, only: '%w'[:index, :show, :new, :create]
 # , only: '%w'[:index, :new, :create]
